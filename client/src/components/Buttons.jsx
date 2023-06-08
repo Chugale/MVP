2739a8e537a82d486834459750e7a7fee7e9845c
Eric Lee <leric7135@gmail.com>
2023-06-07 21:50:27 -0700

    moved buttons to own component for future fix

---
+++ client/src/components/Buttons.jsx
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react';
+
+const Buttons = () => {
+
+  return (
+    <div className="my-8">
+      <button type="button"
+        className="flex text-center text-black-1000 font-semibold bg-gradient-to-r from-red-600 via-blue-600 to-green-600 text-lg px-5 py-1 hover:bg-gradient-to-br focus:border-4 focus:outline-none focus:border-blue-800 w-48">Regular Gems</button>
+      <button type="button"
+        className="flex text-center text-black-1000 font-semibold bg-gradient-to-r from-red-600 via-blue-600 to-green-600 text-lg px-5 py-1 hover:bg-gradient-to-br focus:border-4 focus:outline-none focus:border-blue-800 w-48">Awakened Gems</button>
+    </div>
+  )
+
+
+}
+
+export default Buttons;
